21dd55512f9b1c5cef8900180a4bf3ec9c7b1049
NourSabriEmsi <Nour.Sabri@emsi-edu.ma>
2025-11-13 03:51:33 +0100

    added the support dpf file ml.pdf + Test5 done

---
+++ src/main/java/fr/unica/miage/nours/langchain4j/Test5.java
@@ -0,0 +1,62 @@
+package fr.unica.miage.nours.langchain4j;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.util.Scanner;
+
+public class Test5 {
+    interface Assistant {
+        String chat(String userMessage);
+    }
+
+    public static void main(String[] args) {
+        String llmKey = System.getenv("GEMINI_KEY");
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(llmKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.3)
+                .logRequestsAndResponses(true)
+                .build();
+
+        String nomDocument = "ml.pdf";
+        Document document = FileSystemDocumentLoader.loadDocument(nomDocument);
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+
+        Test4.Assistant assistant =
+                AiServices.builder(Test4.Assistant.class)
+                        .chatModel(model)
+                        .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                        .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                        .build();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+
+}
